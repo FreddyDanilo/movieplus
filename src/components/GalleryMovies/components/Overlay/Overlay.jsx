c3ee807f56c5d93f16b34af498b54fae84ec7b38
Freddy Danilo <fredduddo@gmail.com>
2023-06-11 12:58:44 +0100

    Improved Overlay Component

--- src/components/GalleryMovies/components/Overlay/Overlay.jsx
+++ src/components/GalleryMovies/components/Overlay/Overlay.jsx
@@ -1,16 +1,19 @@
 import { useRef } from "react";
 import styles from "./Overlay.module.css";
-import { useParams } from "react-router-dom";
 
 export const Overlay = () => {
   const overlayRef = useRef();
 
-  window.onmousemove = ({ clientX, clientY }) => {
+  const handleMouseMove = ({ clientX, clientY }) => {
     try {
-      overlayRef.current.style.left = `${clientX - 240}px`;
-      overlayRef.current.style.top = `${clientY - 240}px`;
+      overlayRef.current.style = `
+      left: ${clientX - 240}px; 
+      top: ${clientY - 240}px;
+      `;
     } catch (error) {}
   };
+
+  window.onmousemove = handleMouseMove;
 
   return <div className={styles.overlay} ref={overlayRef}></div>;
 };
