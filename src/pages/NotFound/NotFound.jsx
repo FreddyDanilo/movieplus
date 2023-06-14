8ad16d9ba3cdb264d8eee06aba6f8151527e58e3
Freddy Danilo <fredduddo@gmail.com>
2023-06-14 17:12:59 +0100

    Stylization Page Not Found

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,14 @@
+import { Link } from "react-router-dom";
+import styles from "./NotFound.module.css";
+
 export const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className={styles.not_found}>
+      <span className={styles.number}>404</span>
+      <span className={styles.message}>Ops! Page Not Found</span>
+      <Link className={styles.return} to={"/"}>
+        Return Home
+      </Link>
+    </div>
+  );
 };
