b602cd4db120950752e48d48f6bd50e0695cb397
Freddy Danilo <fredduddo@gmail.com>
2023-06-24 23:44:46 +0100

    Updated About Page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,24 @@
 import styles from "./About.module.css";
+import { Link } from "react-router-dom";
 
 export const About = () => {
-  return <div>About</div>;
+  return (
+    <div className={styles.about}>
+      <span className={styles.title}>
+        This site was developed by{" "}
+        <b>
+          <a href="https://github.com/FreddyDanilo" target="_blank">
+            Freddy Danilo
+          </a>
+        </b>
+      </span>
+      <a
+        className={styles.repository}
+        href="https://github.com/FreddyDanilo/movieplus"
+        target="_blank"
+      >
+        Acess Repository
+      </a>
+    </div>
+  );
 };
